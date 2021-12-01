41cab987ee888032a2722bb723128251280df648
Ressam Al-Thebailah <ralthebailah@MacBook-Air-Ressam.local>
2021-12-01 16:24:35 +0300

    Finished

---
+++   WorldTrotter-Review"Ressam-Reema-Hajar"iter04/WorldTrotter-Review"Ressam-Reema-Hajar"/Controllers/MapViewControllerStoryboard.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewControllerStoryboard.swift
+//  WorldTrotter-Review"Ressam-Reema-Hajar"
+//
+//  Created by Ressam Al-Thebailah on 25/04/1443 AH.
+//
+
+import UIKit
+
+class MapViewControllerStoryboard: UIViewController {
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print("MapViewController loaded its View. ")
+    
+  }
+  
+}
