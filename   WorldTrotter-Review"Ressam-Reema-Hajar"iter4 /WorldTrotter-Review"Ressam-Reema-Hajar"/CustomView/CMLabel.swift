c5afeb1c759ba38bebb82e5bf1e0c49f67c5db36
HAJAR <hajar@HAJARs-MacBook-Pro.local>
2021-12-02 09:10:41 +0300

    Completed World Trotter-iter 4

---
+++   WorldTrotter-Review"Ressam-Reema-Hajar"iter4 /WorldTrotter-Review"Ressam-Reema-Hajar"/CustomView/CMLabel.swift
@@ -0,0 +1,33 @@
+
+
+import UIKit
+
+class CMLabel: UILabel {
+  
+//  override init(frame: CGRect) {
+//    super.init(frame: frame)
+//  }
+
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  init(text: String,
+       textSize: CGFloat,
+       textColor: UIColor = UIColor.black) {
+    super.init(frame: .zero)
+    
+    self.text = text
+    self.font = UIFont.systemFont(ofSize: textSize)
+    self.textColor = textColor
+    
+    custumize()
+  }
+  
+  
+  
+  private func custumize(){
+    translatesAutoresizingMaskIntoConstraints = false
+  }
+}
